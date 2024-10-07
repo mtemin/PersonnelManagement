932c98afd520685dc9367d26c708ea8bdb74255d
Mehmet Emin <mteminn@gmail.com>
2024-10-07 14:00:12 +0300

    set up react router

--- personnel-management-ui/src/main.tsx
+++ personnel-management-ui/src/main.tsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './routes/Home.tsx';
+import ErrorPage from './routes/ErrorPage.tsx';
+import Register from './components/Register.tsx';
+import Login from './components/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      {/* <App /> */}
+    </RouterProvider>
   </StrictMode>,
 )
