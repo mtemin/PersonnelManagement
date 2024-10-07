026357d0718c71435321b1806858f98471202119
Mehmet Emin <mteminn@gmail.com>
2024-10-07 13:59:25 +0300

    add variable

--- personnel-management-ui/tailwind.config.js
+++ personnel-management-ui/tailwind.config.js
@@ -15,6 +15,7 @@ export default {
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        link: 'royalblue',
         card: {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))'
