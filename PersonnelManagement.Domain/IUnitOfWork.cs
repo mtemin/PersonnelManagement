67bbf6025643ba835a5d0923b8f4fb12c37e3113
Mehmet Emin <mteminn@gmail.com>
2024-10-07 14:52:23 +0300

    added interface UnitOfWork

---
+++ PersonnelManagement.Domain/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿
+using PersonnelManagement.Domain.Models;
+using PersonnelManagement.Domain.Models.Abstract;
+using PersonnelManagement.Domain.Models.Concrete;
+using PersonnelManagement.Domain.Repositories;
+
+namespace PersonnelManagement.Domain;
+
+public interface IUnitOfWork : IDisposable
+{
+    IEmployeeRepository Employees { get; }
+    ICompanyRepository Companies { get; }
+    IRepository<Certificate> Certificates { get; }
+    IRepository<LeaveDay> LeaveDays { get; }
+    IRepository<ProfessionalExperience> ProfessionalExperiences { get; }
+    IRepository<Expense> Expenses { get; }
+    Task<int> CommitAsync();
+}
