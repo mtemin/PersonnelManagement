0353b891df8fb0133921951ffbe51dc1d86e2124
Mehmet Emin <mteminn@gmail.com>
2024-10-08 14:55:17 +0300

    create generic IService

---
+++ PersonnelManagement.Domain/Services/IService.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonnelManagement.Domain.Services;
+
+public interface IService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllEntitiesAsync();
+    Task<T> GetEntityByIdAsync(int id);
+    Task<T> CreateEntityAsync(T entity);
+    Task UpdateEntityAsync(T entity);
+    Task DeleteEntityAsync(T entity); 
+}
