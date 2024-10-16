5592b9d550c5aa1950aef2f279cb86272766938a
Mehmet Emin <mteminn@gmail.com>
2024-10-16 13:55:31 +0300

    create abstract and concrete roleservice for adding and removing user role

---
+++ PersonnelManagement.Domain/Services/IRoleService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonnelManagement.Domain.Services;
+
+public interface IRoleService
+{
+    Task AssignRoleToUserAsync(IdentityUser  user, string roleName);
+    Task RemoveRoleFromUserAsync(IdentityUser  user, string roleName);
+    // Add more methods as needed
+}
---
+++ PersonnelManagement.Services/RoleService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PersonnelManagement.Services;
+
+public class RoleService
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public RoleService(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task AssignRoleToUser(IdentityUser user, string roleName)
+    {
+        await _userManager.AddToRoleAsync(user, roleName);
+    }
+}
