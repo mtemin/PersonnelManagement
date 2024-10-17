77df1410b05722ca38581c15d1adab12bffb59fa
Mehmet Emin <mteminn@gmail.com>
2024-10-17 16:57:34 +0300

    create login/register dto

---
+++ PersonnelManagement.API/DTO/RegisterDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonnelManagement.API.DTO;
+
+public class RegisterDTO
+{
+    [Required]
+    [StringLength(maximumLength:50,ErrorMessage = "The {propertyName} must be at least {minimumLength} characters long.", MinimumLength = 4)]
+    [Display(Name = "Username")]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(50, ErrorMessage = "The {propertyName} must be at least {minimumLength} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
