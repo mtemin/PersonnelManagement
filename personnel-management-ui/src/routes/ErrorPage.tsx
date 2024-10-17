dfe28a27540e722f819cae28ed5a9c60769e2595
Mehmet Emin <mteminn@gmail.com>
2024-10-17 19:11:27 +0300

    minor css updates

--- personnel-management-ui/src/routes/ErrorPage.tsx
+++ personnel-management-ui/src/routes/ErrorPage.tsx
@@ -7,11 +7,12 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page" className="bg-red-800 rounded p-8 mx-auto w-[25rem] mt-[25%] translate-y-[-50%]">
-      <h1 className="font-bold text-3xl mb-3">Error!</h1>
-      <p className="mb-3">No page found with this url.</p>
-      <p className="mb-3">
-        <i>{error.statusText || error.message}</i>
+    <div id="error-page" className="bg-red-800 rounded py-8 px-16 mx-auto w-[30rem] mt-[25%] translate-y-[-50%]">
+      <h1 className="font-bold text-3xl mb-6">Error!</h1>
+      <p className="mb-6">No page found with this url.</p>
+      <p className="mb-6">
+          Error :
+          <i> {error.statusText || error.message}</i>
       </p>
       <Button>
         <a href='/'>
