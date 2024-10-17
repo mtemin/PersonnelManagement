01f2473bcf39656f91ab120f1007d51ebb5f103a
Mehmet Emin <mteminn@gmail.com>
2024-10-17 19:09:49 +0300

    add back to homepage link and minor css edits

--- personnel-management-ui/src/routes/Register.tsx
+++ personnel-management-ui/src/routes/Register.tsx
@@ -3,14 +3,17 @@ import { Input } from '@/components/ui/input'
 
 export default function Register() {
   return (
-    <section className='w-[25rem] mt-[25%] m-auto'>
+    <section className='w-[25rem] mt-[25vh] translate-y-[-%50] m-auto'>
+        <h4 className="text-2xl mb-6">Register</h4>
+      <Input className='mb-4' type="text" placeholder="Username" />
       <Input className='mb-4' type="text" placeholder="Name" />
       <Input className='mb-4' type="text" placeholder="Surname" />
       <Input className='mb-4' type="email" placeholder="Email" />
       <Input className='mb-4' type="password" placeholder="Password" />
-      <Button className='mx-auto'>
+      <Button className='mb-4 ml-auto'>
         Register
       </Button>
+        <p>Go back to&nbsp;<a className="text-link underline" href="/">Homepage</a></p>
     </section>
 
   )
