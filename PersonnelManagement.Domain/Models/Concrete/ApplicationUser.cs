b957c645e4d3a97da2ea933920fcde49f8df0c47
Mehmet Emin <mteminn@gmail.com>
2024-10-17 16:59:02 +0300

    minor fix | user id is automatically set  now

--- PersonnelManagement.Domain/Models/Concrete/ApplicationUser.cs
+++ PersonnelManagement.Domain/Models/Concrete/ApplicationUser.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace PersonnelManagement.Domain.Models.Concrete;
 
 public class ApplicationUser  : IdentityUser<string>
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string Id { get; set; }
     public string RoleId { get; set; }
     public Role Role { get; set; }
 }
