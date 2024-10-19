d057cd71e808863d04211c2758c894cffbb8fd8d
Mehmet Emin <mteminn@gmail.com>
2024-10-19 17:51:52 +0300

    new responsive navbar

--- personnel-management-ui/src/components/Navbar.tsx
+++ personnel-management-ui/src/components/Navbar.tsx
@@ -1,29 +1,63 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav className="border-b-foreground border-b-2">
-            <ul className="flex items-center justify-center container mx-auto">
-                <li className=" justify-center items-center">
-                <img src="/src/assets/react.svg" alt="site logo"/>
-                </li>
-                <li className="px-6 py-3 hover:text-link">
-                    <a href="/" className="">Home</a>
-                </li>
-                <li className="px-6 py-3 hover:text-link">
-                    <a href="/companies" className="">Companies</a>
-                </li>
-                <li className="px-6 py-3 hover:text-link">
-                    <a href="/employees" className="">Employees</a>
-                </li>
-                <li className="px-6 py-3 hover:text-link">
-                    <a href="/'+2sfd2d_" className="">Error page</a>
-                </li>
-                <li className="px-6 py-3 hover:text-link ml-auto">
-                    <a href="/profile" className="">Profile</a>
-                </li>
-            </ul>
+    const [navbarOpen, setNavbarOpen] = useState(false);
 
+    const handleNavbarVisibility = () => {
+        setNavbarOpen(!navbarOpen);
+    };
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="container flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="/src/assets/react.svg" className="h-8" alt="React Logo"/>
+                    <span
+                        className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Personnel Management</span>
+                </a>
+                <button onClick={handleNavbarVisibility}
+                    data-collapse-toggle="navbar-default" type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+                <div className={`${!navbarOpen && "hidden"} w-full md:block md:w-auto duration-500 " id="navbar-default`}>
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <li>
+                            <a href="/"
+                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                               >Home</a>
+                        </li>
+                        <li>
+                            <a href="/companies"
+                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                Companies</a>
+                        </li>
+                        <li>
+                            <a href="/employees"
+                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                Employees</a>
+                        </li>
+                        <li>
+                            <a href="/expenses"
+                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                Expenses</a>
+                        </li>
+                        <li>
+                            <a href="/leavedays"
+                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                Leave Days</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
+
     );
 }
 
