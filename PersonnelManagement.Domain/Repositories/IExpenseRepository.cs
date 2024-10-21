23221b74f84ac575f75f2c28a49edd7381242d84
Mehmet Emin <mteminn@gmail.com>
2024-10-21 21:50:04 +0300

    create ExpenseRepository and related interface&classes

---
+++ PersonnelManagement.Domain/Repositories/IExpenseRepository.cs
@@ -0,0 +1,8 @@
+﻿using PersonnelManagement.Domain.Models.Abstract;
+
+namespace PersonnelManagement.Domain.Repositories;
+
+public interface IExpenseRepository :IRepository<Expense>
+{
+    
+}
---
+++ PersonnelManagement.Services/ExpenseService.cs
@@ -0,0 +1,12 @@
+﻿using PersonnelManagement.Domain;
+using PersonnelManagement.Domain.Models.Abstract;
+using PersonnelManagement.Domain.Repositories;
+
+namespace PersonnelManagement.Services;
+
+public class ExpenseService:BaseService<Expense>
+{
+    public ExpenseService(IUnitOfWork unitOfWork, IRepository<Expense> repository) : base(unitOfWork, repository)
+    {
+    }  
+}
