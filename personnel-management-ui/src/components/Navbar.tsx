cb6a6bdfa809d511245cc479b3e1adcd15caa234
Mehmet Emin <mteminn@gmail.com>
2024-10-22 02:08:44 +0300

    Update Navbar.tsx

--- personnel-management-ui/src/components/Navbar.tsx
+++ personnel-management-ui/src/components/Navbar.tsx
@@ -43,16 +43,6 @@ function Navbar() {
                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Employees</a>
                         </li>
-                        <li>
-                            <a href="/expenses"
-                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Expenses</a>
-                        </li>
-                        <li>
-                            <a href="/leavedays"
-                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                Leave Days</a>
-                        </li>
                     </ul>
                 </div>
             </div>
