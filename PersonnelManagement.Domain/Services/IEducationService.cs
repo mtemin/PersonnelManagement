8f991895116ede823b61c5d1d2f8003ed631dd27
Mehmet Emin <mteminn@gmail.com>
2024-10-21 23:34:36 +0300

    update education service

--- PersonnelManagement.Domain/Services/IEducationService.cs
+++ PersonnelManagement.Domain/Services/IEducationService.cs
@@ -4,5 +4,8 @@ namespace PersonnelManagement.Domain.Services;
 
 public interface IEducationService:IService<Education>
 {
-    
+    Task<Employee> UpdateEntityAsync(Education educationToBeUpdated, Education education);
+    Task<IEnumerable<Education>> GetEducationsByEmployeeIdAsync(int employeeId);
+
+
 }
--- PersonnelManagement.Services/EducationService.cs
+++ PersonnelManagement.Services/EducationService.cs
@@ -1,12 +1,33 @@
 ﻿using PersonnelManagement.Domain;
 using PersonnelManagement.Domain.Models.Concrete;
 using PersonnelManagement.Domain.Repositories;
+using PersonnelManagement.Domain.Services;
 
 namespace PersonnelManagement.Services;
 
-public class EducationService:BaseService<Education>
+public class EducationService:BaseService<Education>, IEducationService
 {
-    public EducationService(IUnitOfWork unitOfWork, IRepository<Education> repository) : base(unitOfWork, repository)
+    private readonly IUnitOfWork unitOfWork;
+    private IEducationService _educationServiceImplementation;
+
+    public EducationService(IUnitOfWork _unitOfWork, IRepository<Education> repository)
+        : base(_unitOfWork, repository)
     {
+        unitOfWork = _unitOfWork;
+    }
+
+    public async Task<IEnumerable<Education>> GetEducationsByEmployeeIdAsync(int employeeId)
+    {
+        return unitOfWork.Educations.Find(e => e.EmployeeId == employeeId);
+    }
+
+    public Task<Employee> UpdateEntityAsync(Education educationToBeUpdated, Education education)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Employee>> GetEducationByEmployeeIdAsync(int employeeId)
+    {
+        throw new NotImplementedException();
     }
 }
