81af32fd609409a92de9d68da4bcd7dd98ae418f
Mehmet Emin <mteminn@gmail.com>
2024-10-21 23:35:39 +0300

    Update ILeaveDayService.cs

--- PersonnelManagement.Domain/Services/ILeaveDayService.cs
+++ PersonnelManagement.Domain/Services/ILeaveDayService.cs
@@ -4,5 +4,9 @@ namespace PersonnelManagement.Domain.Services;
 
 public interface ILeaveDayService:IService<LeaveDay>
 {
-    
+    Task<LeaveDay> UpdateEntityAsync(LeaveDay leaveDayToBeUpdated, LeaveDay leaveDay);
+    Task<IEnumerable<LeaveDay>> GetLeaveDaysByCompanyIdAsync(int companyId);
+    Task<IEnumerable<LeaveDay>> GetLeaveDaysByEmployeeIdAsync(int employeeId);
+
+
 }
