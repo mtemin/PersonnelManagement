4d8564b6e39bc0984605c6ea99e0d29f51936511
Mehmet Emin <mteminn@gmail.com>
2024-10-21 23:34:16 +0300

    update expense service

--- PersonnelManagement.Domain/Services/IExpenseService.cs
+++ PersonnelManagement.Domain/Services/IExpenseService.cs
@@ -4,5 +4,9 @@ namespace PersonnelManagement.Domain.Services;
 
 public interface IExpenseService:IService<Expense>
 {
-    
+    Task<Expense> UpdateEntityAsync(Expense expenseToBeUpdated, Expense expense);
+    Task<IEnumerable<Expense>> GetExpensesByCompanyIdAsync(int companyId);
+    Task<IEnumerable<Expense>> GetExpensesByEmployeeIdAsync(int employeeId);
+
+
 }
--- PersonnelManagement.Services/ExpenseService.cs
+++ PersonnelManagement.Services/ExpenseService.cs
@@ -1,12 +1,33 @@
 ﻿using PersonnelManagement.Domain;
 using PersonnelManagement.Domain.Models.Abstract;
 using PersonnelManagement.Domain.Repositories;
+using PersonnelManagement.Domain.Services;
 
 namespace PersonnelManagement.Services;
 
-public class ExpenseService:BaseService<Expense>
+public class ExpenseService:BaseService<Expense>, IExpenseService
 {
-    public ExpenseService(IUnitOfWork unitOfWork, IRepository<Expense> repository) : base(unitOfWork, repository)
+    private readonly IUnitOfWork unitOfWork;
+
+    public ExpenseService(IUnitOfWork _unitOfWork, IRepository<Expense> repository)
+        : base(_unitOfWork, repository)
     {
-    }  
+        unitOfWork = _unitOfWork;
+    }
+
+    public Task<Expense> UpdateEntityAsync(Expense expenseToBeUpdated, Expense expense)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Expense>> GetExpensesByCompanyIdAsync(int companyId)
+    {
+        return unitOfWork.Expenses.Find(e => e.CompanyId == companyId);
+    }
+    
+    public async Task<IEnumerable<Expense>> GetExpensesByEmployeeIdAsync(int employeeId)
+    {
+        return unitOfWork.Expenses.Find(e => e.EmployeeId == employeeId);
+    }
+    
 }
