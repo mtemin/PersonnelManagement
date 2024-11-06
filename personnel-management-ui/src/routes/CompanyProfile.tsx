ac1066ca8a3990501de3c3dee77a63e37720fa90
Mehmet Emin <mteminn@gmail.com>
2024-11-06 20:44:11 +0300

    update design layout on CompanyProfile route

--- personnel-management-ui/src/routes/CompanyProfile.tsx
+++ personnel-management-ui/src/routes/CompanyProfile.tsx
@@ -1,55 +1,42 @@
 import * as domain from "domain";
 import { useParams } from "react-router-dom";
-import useCompanyQuery from "@/hooks/useCompanyQuery.ts";
-import CustomTable from "@/components/CustomTable.tsx";
-import useEmployeesByCompanyIdQuery from "@/hooks/useEmployeesByCompanyId.ts";
-import {Table, TableBody, TableHead, TableHeader, TableRow} from "@/components/ui/table.tsx";
-import TableItemEmployee from "@/components/TableItemEmployee.tsx";
+import useItemQuery, {useCompanyQuery} from "@/hooks/useCompanyQuery.ts";
+import EmployeeTable from "@/components/EmployeeTable.tsx";
+import useEmployeesByCompanyIdQuery, {useItemsByCompanyIdQuery} from "@/hooks/useItemsByCompanyId.ts";
+
 function CompanyProfile() {
     const  {companyId}  = useParams();
     const {data,isLoading,isError}:{data:Company,isLoading:any,isError:any} = useCompanyQuery(companyId);
-    const {data: employeesData, isLoading: employeesIsLoading, isError: employeesIsError} = useEmployeesByCompanyIdQuery(data?.companyId);
+    const {data: employeesData, isLoading: employeesIsLoading, isError: employeesIsError} = useItemsByCompanyIdQuery("Employee",data?.companyId);
+
     if (isLoading) return <div>Loading company...</div>;
     if(employeesIsLoading) return <div>Loading employees of {data?.name} ...</div>;
+
     if (isError) return <div>Error fetching company</div>;
     if(employeesIsError) return <div>Error fetching employees of {data?.name}</div>;
-    console.log(employeesData)
+
 
     return (
-        <main className="container font-medium mx-auto mt-10">
-            <h3 className="text-4xl">
+        <main className="container font-medium mx-auto mt-10 ">
+            <h3 className="text-4xl mb-8">
             {data?.name}
             </h3>
-            <section>
-                <p className="text-xl">Employees</p>
-
-                    <Table>
-                        <TableHeader>
-                            <TableRow>
-                                {/*<TableHead className="hidden w-[100px] sm:table-cell">*/}
-                                {/*    <span className="sr-only">img</span>*/}
-                                {/*</TableHead>*/}
-                                <TableHead>Name</TableHead>
-                                <TableHead>Surname</TableHead>
-                                <TableHead className="hidden md:table-cell">
-                                    Company
-                                </TableHead>
-                                <TableHead className="hidden md:table-cell">
-                                    Job Title
-                                </TableHead>
-                                <TableHead>
-                                    <span className="sr-only">Actions</span>
-                                </TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {employeesData && employeesData?.map((item:any,index:number) =>
-                                <TableItemEmployee key={index} item={item}/>
-                            )}
-
-                        </TableBody>
-                    </Table>
-            </section>
+            <div className="flex h-[40rem]">
+                <section className="p-5 border border-border rounded overflow-scroll mr-2 ">
+                    <p className="text-xl">Employees</p>
+                    <EmployeeTable key={employeesData?.id} itemCollection={employeesData}/>
+                </section>
+            <div className="flex flex-col ml-2 max-h-[100vh]">
+                <section className="bg-card border border-border p-5 rounded overflow-scroll mb-4">
+                    <p className="text-xl">Expenses</p>
+                    <EmployeeTable key={employeesData?.id} itemCollection={employeesData}/>
+                </section>
+                <section className="bg-card border border-border p-5 rounded overflow-scroll">
+                    <p className="text-xl">Leave days</p>
+                    <EmployeeTable key={employeesData?.id} itemCollection={employeesData}/>
+                </section>
+            </div>
+            </div>
         </main>
     );
 }
